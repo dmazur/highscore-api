0cd52583e1816abe5304c02a7eef4e069daeb09b
Dawid Mazur <d.mazur@clearcode.cc>
2020-04-14 15:44:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Highscore API
+=============
+
+A *very* messy API for keeping a high score on a Construct project for programming classes with children.  
+Full of bad practices, so you have been warned. Most likely will never be refactored.
---
+++ index.js
@@ -0,0 +1,83 @@
+const http = require('http');
+const querystring = require('querystring');
+
+require('dotenv').config()
+const lowdb = require('lowdb');
+const FileAsync = require('lowdb/adapters/FileAsync');
+
+const hostname = process.env.HOST;
+const port = process.env.PORT;
+
+const adapter = new FileAsync('db.json');
+
+(async () => {
+  const db = await lowdb(adapter);
+  const server = http.createServer((req, res) => {
+    setResponseHeaders(res, parseInt(process.env.CORS));
+
+    if (req.method === 'GET') {
+      let scoresCollection = db.get('scores');
+
+      if (parseInt(process.env.ONLY_APPROVED)) {
+        scoresCollection = scoresCollection.filter({approved: true});
+      }
+
+      const topScores = scoresCollection
+        .orderBy(['score'], ['desc'])
+        .take(10)
+        .value();
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/json');
+      res.end(JSON.stringify(topScores));
+    } else if (req.method === 'POST') {
+      let body = '';
+
+      req.on('data', data => {
+        body += data;
+      });
+
+      req.on('end', () => {
+        const postData = querystring.parse(body);
+        const name = postData.name;
+        const score = parseInt(postData.score);
+
+        if (!name || !score) {
+          res.statusCode = 400;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('Missing data');
+        }
+
+        db.get('scores')
+          .push({
+            name,
+            score,
+            date: Date.now(),
+            approved: false,
+          })
+          .write()
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('OK');
+      });
+    } else {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Method not allowed');
+    }
+  });
+
+  db.defaults({ scores: [] }).write();
+  
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+})();
+
+function setResponseHeaders(response, cors = true) {
+  if (!cors) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  }
+} 
